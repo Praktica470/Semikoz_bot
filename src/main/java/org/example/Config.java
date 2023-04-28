4119639c2862521965b8cff519f644383af0e810
Praktica470 <132103136+Praktica470@users.noreply.github.com>
2023-04-29 00:11:39 +0300

    Update Config.java

--- src/main/java/org/example/Config.java
+++ src/main/java/org/example/Config.java
@@ -1,6 +1,6 @@
 package org.example;
 
 public class Config {
-public static final String TOKEN = "MTA3MjEyNTM3NzM2NDg4OTY4MA.GULvrw.kNpxzExwOiYC8QA83DIqykxqlTr5095F5xOBjY";
+public static final String TOKEN = "token";
 public static final String PREFIX = "!";
 }
